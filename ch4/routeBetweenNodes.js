930cefa5a6471c71b97de3b451f28919d8c13c8b
Eric M Sandoval <ericmsandoval@gmail.com>
2018-06-04 15:14:25 -0700

    init ch4 and routeBetweenGraphNodes

---
+++ ch4/routeBetweenNodes.js
@@ -0,0 +1,44 @@
+/* 
+  Given a directed graph, design an algorithm to find out whether there is a route between nodes
+
+  Below is a "code skeleton" of a breadth first search - there are several assumptions
+*/
+
+const Queue; // require/import a Queue
+
+const routeBetweenGraphNodes = (graph, startNode, endNode) => {
+  if (startNode === endNode) {
+    return true;
+  }
+  let q = new Queue;
+
+  startNode.state = 'visiting';
+  q.add(startNode); 
+
+  while (q.length > 0) { // q.length or q.isEmpty() to check queue, depends on Queue implementation
+    let currNode = q.dequeue();
+    for (let adjNode in currNode.edges) {
+      if (adjNode.state === 'unvisited') {
+        if (adjNode === endNode) {
+          return true;
+        }
+        adjNode.state = 'visiting';
+        q.add(adjNode);
+      }
+    }
+
+    currNode.state = 'visited';
+  }
+
+  return false;
+}
+
+/* 
+  ANALYSIS
+
+  The above assumes graph nodes are initialized with a state property of 'unvisited'. Otherwise we could traverse all elements
+  in the graph and flag them as such. 
+
+  The time complexity here is O(V + E), where V is total vertices (objects in the graph) and E is total number of Edges (connections)
+  between vertices. In the worst case we'd have to do O(V + E) operations (constants dropped). 
+*/
